26d892686d6d20a3bb2fdbe451938b70294cea20
Tamas Sule <xjmdoo@gmail.com>
2020-10-21 19:50:16 +0200

    Add some comments

--- run-docker.sh
+++ run-docker.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+
+# ~/ctf is a directory which will be available inside the container
+# Port 8888 is exposed for remote debugging
 docker run --name ctfbox -v ~/ctf:/ctf -p 8888:8888 -t -d ctfbox:1.0
